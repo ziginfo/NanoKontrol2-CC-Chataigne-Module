da8f513cb38153f8db0ec29c406042831cd7be31
ziginfo <34913387+ziginfo@users.noreply.github.com>
2023-10-22 17:16:46 +0200

    Add files via upload

--- nanokontrolcc.js
+++ nanokontrolcc.js
@@ -7,26 +7,34 @@ Connect the USB cable from your computer to the nanoKONTROL2 and engage CC mode.
 //Request MIDI Control Change message
 function ccEvent(channel, number, value)
 {  
+
+
  //Rotaries
-	if(number >= 16 && number <= 23){
+		if(number >= 16 && number <= 23){
        		var index = number-16;
-       		local.values.strips.getChild('Strip '+(index+1)).rotaryValue.set(value);    }            
+       		local.values.strips.getChild('Strip '+(index+1)).rotaryValue.set(value);    }
+            
+            
  //Faders            
          if(number >= 0 && number <= 7){
         	var index = number;
-            	local.values.strips.getChild('Strip '+(index+1)).faderValue.set(value);    }        
+            local.values.strips.getChild('Strip '+(index+1)).faderValue.set(value);    }
+        
 //Rec Button
-	if (number >= 64 && number <= 71){
+		if (number >= 64 && number <= 71){
         	var index = number-64;
-       		local.values.strips.getChild('Strip '+(index+1)).rec.set(value); }             
+       		local.values.strips.getChild('Strip '+(index+1)).rec.set(value); } 
+            
 //Solo Button
-	if (number >= 32 && number <= 39){
+		if (number >= 32 && number <= 39){
         	var index = number-32;
-		local.values.strips.getChild('Strip '+(index+1)).solo.set(value); }            
+			local.values.strips.getChild('Strip '+(index+1)).solo.set(value); }
+            
 //Mute Button
-	if (number >= 48 && number <= 55){
+		if (number >= 48 && number <= 55){
         	var index = number-48;
-       		local.values.strips.getChild('Strip '+(index+1)).mute.set(value);  }             
+       		local.values.strips.getChild('Strip '+(index+1)).mute.set(value);  } 
+            
 //Transport
     	if (number == 43) {local.values.transport.rewind.set(value);}
         if (number == 44) {local.values.transport.forward.set(value);}
@@ -42,5 +50,12 @@ function ccEvent(channel, number, value)
         if (number == 61) {local.values.misc.markLeft.set(value);}
         if (number == 62) {local.values.misc.markRight.set(value);}     
         
+}
+
+//common functions
+
+
+function send_control(channel, number, value) {
+	local.sendCC (channel, number, value);
 }
 
